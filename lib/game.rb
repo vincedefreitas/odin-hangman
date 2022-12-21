5bbdc06a90202f9619edeae394b1ea5173409899
Vince De Freitas <97405426+vincedefreitas@users.noreply.github.com>
2022-12-21 12:50:01 +0000

    Create hidden word method

--- lib/game.rb
+++ lib/game.rb
@@ -6,14 +6,29 @@ class Game
     @master_word = []
     @hidden_word = []
     @guessed_letters = []
+    @incorrect_guesses_left = 8
   end
 
-  def select_random_word
+  def select_master_word
     new_word_list = []
     File.open('word_list.txt', 'r') do |file|
       file.readlines.each do |line|
         new_word_list.push(line) if (line.length >= 6 && line.length <= 13)
       end
     end
-    new_word_list.sample.chomp
+    @master_word = new_word_list.sample.chomp.split('')
   end
+
+  def create_hidden_word
+    @hidden_word = Array.new(@master_word.length, '_')
+  end
+
+
+end
+
+game = Game.new
+game.select_master_word
+game.create_hidden_word
+p game.master_word
+p game.hidden_word
+
