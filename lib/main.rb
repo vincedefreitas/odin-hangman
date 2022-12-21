803d61b628ce94c42671836d3665213649c23fe5
Vince De Freitas <97405426+vincedefreitas@users.noreply.github.com>
2022-12-21 12:20:31 +0000

    Add win scenario

---
+++ lib/main.rb
@@ -0,0 +1,45 @@
+def select_random_word
+  new_word_list = []
+  File.open('word_list.txt', 'r') do |file|
+    file.readlines.each do |line|
+      new_word_list.push(line) if (line.length >= 5 && line.length <= 12)
+    end
+  end
+  new_word_list.sample.chomp
+end
+
+# alphabet = ('a'..'z').to_a
+master_word = select_random_word.split('')
+hidden_word = Array.new(master_word.length, '_')
+guessed_letters = []
+game_over = false
+
+puts "Welcome to hangman!"
+p master_word
+p hidden_word
+
+until game_over
+  puts hidden_word.join
+  puts "\nGuess a letter"
+  guess = gets.chomp
+
+  if master_word.include?(guess)
+    master_word.each_with_index do | letter, index |
+      if guess == letter
+        hidden_word[index] = letter
+      end
+    end
+  else
+    guessed_letters.push(guess)
+    puts "\nIncorrect!"
+    puts "\nLetters guessed: #{guessed_letters.join}"
+  end
+
+  if hidden_word == master_word
+    puts hidden_word.join
+    puts "\nCongratulations, you figured out the secret word!"
+    game_over = true
+  end
+end
+
+
